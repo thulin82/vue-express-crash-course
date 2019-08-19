af6b51a2c53bf27253161ba35f9d9ca6fd4d7af5
Markus Thulin <macky_b@hotmail.com>
2019-08-19 23:01:49 +0200

    Whoops, typo

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.use('/api/posts', posts);
 // Handle production
 if (process.env.NODE_ENV === 'production') {
     // Static folder
-    app.use/(express.static(__dirname + '/public/'));
+    app.use(express.static(__dirname + '/public/'));
 
     // Handle SPA
     app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
